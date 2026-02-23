125c2dec493a5d27d46437153bbf3c8efd6431f3
Larry1280326 <khliuae@connect.ust.hk>
2026-02-24 00:22:59 +0800

    fix: minor fix in schema

--- src/upload/schemas.py
+++ src/upload/schemas.py
@@ -13,7 +13,7 @@ class DocumentItem(BaseModel):
     timestamp: Optional[datetime] = None
     volume: Optional[float] = None
     screen_on_ratio: Optional[float] = None
-    wifi_connected: Optional[str] = None
+    wifi_connected: Optional[bool] = None
     wifi_ssid: Optional[str] = None
     network_traffic: Optional[float] = None
     Rx_traffic: Optional[float] = None
